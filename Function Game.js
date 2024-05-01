085c2ed7cbd8450d522faa07f8c32c620461216b
Joel Meyer <14joelmsis.gl@Joels-MacBook-Air.local>
2024-05-01 18:04:58 +0200

    firtnite

--- Function Game.js
+++ Function Game.js
@@ -20,3 +20,25 @@ const mapharhar = () => {
 
    return generatedMap
 }
+
+
+// 1' create function that puts swag messiah and legalized nuclear bombs on the map
+// 1.b call maphahar and store it in variable
+// 1.c we modify that variable
+const swagmessiah = () => {
+  var moistCr1t1kal = mapharhar();
+
+  var swagPosition = [0,0]
+
+  const name = "swagmessiah";
+  moistCr1t1kal[swagPosition[0], swagPosition[1]] = name
+  console.log("SWAG MESSIAH IS IN THE HOUSE 🔥 🔥 🔥")
+  const legalizeNuclearBombs = (mapo) => {
+    var nuclearPosition = [3,3]
+    const nname = "legalizeNuclearBombs"
+    mapo[nuclearPosition[0]][nuclearPosition[1]] = nname
+  }
+  legalizeNuclearBombs(moistCr1t1kal)
+
+}
+mapharhar()
